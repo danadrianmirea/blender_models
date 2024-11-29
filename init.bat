873e7b413c044618becc024f934c8d9e403198db
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-30 00:58:03 +0200

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# blender_models" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/blender_models.git
-git push -u origin main
